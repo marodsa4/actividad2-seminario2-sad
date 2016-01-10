f365a0afbb3159df1640c2e36dd7be7da071223c
Maria Angeles Rodriguez Sanchez <marodsa4@inf.upv.es>
2016-01-10 22:43:00 +0100

    Servidor con fichero fijo Tag 0.2

--- act2_sem2.js
+++ act2_sem2.js
@@ -1,8 +1,22 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
+    // var fichero = 'HelloWorlxczcd.txt'; // para que salte error
+    var fichero = 'HelloWorld.txt';
+
+	fs.readFile(fichero, function (error, datos_fichero) {
+        if (error) {
+            response.write(error + "\n");
+            response.end();
+            console.log('Ha ocurrido un error: ' + error);
+        }
+        else {
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.end(datos_fichero);
+            console.log('Fichero leido correctamente');
+        }
+    });
 }).listen(1337, "127.0.0.1");
 
 console.log('Server running at http://127.0.0.1:1337/');
